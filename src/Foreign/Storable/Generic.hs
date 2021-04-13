3117c5b5eee93659c2d945663f37babbee30d768
Alexander Bondarenko <aenor.realm@gmail.com>
2021-04-13 13:25:41 +0300

    Added a non-internal getFilling helper.

--- src/Foreign/Storable/Generic.hs
+++ src/Foreign/Storable/Generic.hs
@@ -17,9 +17,30 @@ Portability : portable
 {-#LANGUAGE UndecidableInstances #-}
 
 
-module Foreign.Storable.Generic (GStorable (..), Storable(..)) where
+module Foreign.Storable.Generic (GStorable (..), Storable(..), getFilling) where
 
 
 
 import Foreign.Storable (Storable(..))
-import Foreign.Storable.Generic.Internal (GStorable (..))
+import GHC.Generics
+
+import Foreign.Storable.Generic.Internal (GStorable (..), GStorable' (..))
+import qualified Foreign.Storable.Generic.Tools as Tools
+
+{- | A helper to visualize layout.
+
+>>> data Foo = Foo Word8 Float Word32 Float deriving (Show, Generic, GStorable)
+>>> getFilling (Foo 0 0 0 0)
+[Size 1,Padding 3,Size 4,Size 4,Size 4]
+
+Nested structures are opaque to their parents:
+
+>>> data Bar = Bar Foo Double Double
+>>> getFilling $ Bar (Foo 0 0 0 0) 0 0
+[Size 16,Size 8,Size 8]
+-}
+getFilling :: (Generic a, GStorable a, GStorable' (Rep a)) => a -> [Tools.Filling]
+getFilling x = Tools.getFilling $ zip sizes aligns
+    where sizes  = glistSizeOf'    gx
+          aligns = glistAlignment' gx
+          gx = from x
